8dda53ed210ff9ca896de25b418f46b11a4e2723
Luc Fouin <luc.fouin@gmail.com>
2012-11-02 14:26:01 +0100

    bkp des confs avant creation des liens

--- install.sh
+++ install.sh
@@ -2,9 +2,13 @@
 
 cd ~
 
+mv .zshrc .zshrc.old-my-conf
 ln -s my-conf/zshrc .zshrc
 
+mv .vim .vim.old-my-conf
+mv .vimrc .vimrc.old-my-conf
 ln -s my-conf/vim .vim
 ln -s my-conf/vimrc .vimrc
 
+mv .gitconfig .gitconfig.old-my-conf
 ln -s my-conf/gitconfig .gitconfig
